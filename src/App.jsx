02da63e4c69a8a4c1da063be7b9f33f540ec3889
zakzaks <ariefn82@gmail.com>
2026-04-03 14:25:22 +0700

    refactor: enhance App component with geolocation and sorting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,93 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
-  const modal = useRef();
-  const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+	const modal = useRef();
+	const selectedPlace = useRef();
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+	const [pickedPlaces, setPickedPlaces] = useState([]);
 
-  function handleStartRemovePlace(id) {
-    modal.current.open();
-    selectedPlace.current = id;
-  }
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const sortedPlaces = sortPlacesByDistance(
+				AVAILABLE_PLACES,
+				position.coords.latitude,
+				position.coords.longitude,
+			);
 
-  function handleStopRemovePlace() {
-    modal.current.close();
-  }
+			setAvailablePlaces(sortedPlaces);
+		});
+	}, []);
 
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-  }
+	function handleStartRemovePlace(id) {
+		modal.current.open();
+		selectedPlace.current = id;
+	}
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    modal.current.close();
-  }
+	function handleStopRemovePlace() {
+		modal.current.close();
+	}
 
-  return (
-    <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
+	function handleSelectPlace(id) {
+		setPickedPlaces((prevPickedPlaces) => {
+			if (prevPickedPlaces.some((place) => place.id === id)) {
+				return prevPickedPlaces;
+			}
+			const place = AVAILABLE_PLACES.find((place) => place.id === id);
+			return [place, ...prevPickedPlaces];
+		});
 
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
-        />
-      </main>
-    </>
-  );
+		const storeIds = JSON.parse(localStorage.getItem("selectedPlace") || []);
+		if (storeIds.indexof(id) === -1) {
+			localStorage.setItem("selectedPlace", JSON.stringify([id, ...storeIds]));
+		}
+	}
+
+	function handleRemovePlace() {
+		setPickedPlaces((prevPickedPlaces) =>
+			prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
+		);
+		modal.current.close();
+	}
+
+	return (
+		<>
+			<Modal ref={modal}>
+				<DeleteConfirmation
+					onCancel={handleStopRemovePlace}
+					onConfirm={handleRemovePlace}
+				/>
+			</Modal>
+
+			<header>
+				<img src={logoImg} alt="Stylized globe" />
+				<h1>PlacePicker</h1>
+				<p>
+					Create your personal collection of places you would like to visit or
+					you have visited.
+				</p>
+			</header>
+			<main>
+				<Places
+					title="I'd like to visit ..."
+					fallbackText={"Select the places you would like to visit below."}
+					places={pickedPlaces}
+					onSelectPlace={handleStartRemovePlace}
+				/>
+				<Places
+					title="Available Places"
+					places={AVAILABLE_PLACES}
+					onSelectPlace={handleSelectPlace}
+				/>
+			</main>
+		</>
+	);
 }
 
 export default App;
