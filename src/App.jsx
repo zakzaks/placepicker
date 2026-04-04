cf5bf5be0ce7f9fed3ca1ad14f3d78b2461f825d
zakzaks <ariefn82@gmail.com>
2026-04-04 16:15:24 +0700

    refactor: enhance DeleteConfirmation component with timer state and progress indication

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -54,7 +54,7 @@ function App() {
 		}
 	}
 
-	function handleRemovePlace() {
+	const handleRemovePlace = useCallback(function handleRemovePlace() {
 		setPickedPlaces((prevPickedPlaces) =>
 			prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
 		);
@@ -65,7 +65,7 @@ function App() {
 			"selectedPlace",
 			JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current)),
 		);
-	}
+	}, []);
 
 	return (
 		<>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+	const [remainingTime, setRemainingTime] = useState(TIMER);
+
+	useEffect(() => {
+		console.log("INTERVAL");
+		const interval = setInterval(() => {
+			setRemainingTime((prevTime) => {
+				return prevTime - 10;
+			});
+		}, 10);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			onConfirm();
-		}, 3000);
+		}, TIMER);
 
 		return () => {
 			clearTimeout(timer);
@@ -23,6 +40,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
 					Yes
 				</button>
 			</div>
+			<progress value={remainingTime} max={TIMER}></progress>
 		</div>
 	);
 }
